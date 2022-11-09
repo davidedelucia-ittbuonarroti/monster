2658ad729333eacb07e641a14b7c3018618e267c
DAVIDE.DELUCIA <DAVIDE.DELUCIA@buonarroti.tn.it>
2022-11-09 16:12:21 +0100

    feat: creazione classe monster e inserimento atttributi

---
+++ src/com/company/Monster.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Monster {
+    private String nome;
+    private int puntiFerita;
+    private int danno;
+
+    //costruttore
+    //metodo attacco
+    //metodo subisci
+    //toString
+    //git push origin main
+}
