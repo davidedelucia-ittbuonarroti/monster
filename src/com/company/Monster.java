e9812db19b5fa23b2daa348368af049cb8b92a0d
Davide De Lucia <davide.delucia@buonarroti.tn.it>
2022-11-15 18:23:48 +0100

    feat: aggiunto metodo costruttore alla classe Monster

--- src/com/company/Monster.java
+++ src/com/company/Monster.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.util.Random;
 
 public class Monster {
     private String nome;
@@ -6,6 +7,12 @@ public class Monster {
     private int danno;
 
     //costruttore
+    public Monster(String nome, int puntiFerita){
+        Random r = new Random();
+        this.nome = nome;
+        this.puntiFerita = puntiFerita;
+        this.danno = r.nextInt(puntiFerita);
+    }
     //metodo attacco
     //metodo subisci
     //toString
