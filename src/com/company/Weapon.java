32f2d58ed76ac78e420a55843a4455cd44c1a2e6
DAVIDE.DELUCIA <DAVIDE.DELUCIA@buonarroti.tn.it>
2022-11-16 16:09:46 +0100

    feat: aggiunto costruttore e attributi alla classe Weapon

--- src/com/company/Weapon.java
+++ src/com/company/Weapon.java
@@ -1,5 +1,12 @@
 package com.company;
 
 public class Weapon {
+    private boolean equipped;
+    private int arma;
+
+    public Weapon(int danni){
+        arma = danni;
+        equipped = false;
+    }
     
 }
